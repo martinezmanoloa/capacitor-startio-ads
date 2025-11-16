f2b45bff6fe604f6354a5747969520920c569398
Manolo Martínez <martinezmanoloa@gmail.com>
2025-11-15 20:16:47 -0400

    Removed example-app

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { StartioAds } from '@martinezmanoloa/capacitor-startio-ads';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    StartioAds.echo({ value: inputValue })
-}
